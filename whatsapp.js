f2bc97f788bb0136b790a9a13abc6bf6999db1d8
Kelvin-dev001 <kelvinoyugi101@gmail.com>
2025-12-24 10:08:29 +0300

    sendmsgpatch

--- whatsapp.js
+++ whatsapp.js
@@ -1,12 +1,13 @@
 import axios from 'axios';
 
 const WHATSAPP_TOKEN = process.env.WHATSAPP_TOKEN;
+const WA_PHONE_NUMBER_ID = process.env.WA_PHONE_NUMBER_ID; // set this in env
 
 export async function sendWhatsAppMessage(to, body) {
-  const url = 'https://graph.facebook.com/v19.0/me/messages';
+  const url = `https://graph.facebook.com/v19.0/${WA_PHONE_NUMBER_ID}/messages`;
   await axios.post(url, {
     messaging_product: 'whatsapp',
-    to,
+    to, // e.g., "254759293030" (no plus) or your recipient E.164 without +
     text: { body }
   }, {
     headers: {
